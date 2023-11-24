122794f7b18edc1641a8672f538387f81f6d50ec
arajo <i_tomorrow_i@hanmail.net>
2023-11-24 11:03:37 +0900

    Refactoring Logger

--- logger.py
+++ logger.py
@@ -1,16 +1,26 @@
 import logging
 from logging import getLogger
 
-################################################################
-# Set logger
-simple_formatter = logging.Formatter("[%(name)s] %(message)s")
-complex_formatter = logging.Formatter(
-    "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] - %(message)s"
-)
 
-app_logger = getLogger()
-handler = logging.StreamHandler()
-handler.setFormatter(complex_formatter)
-app_logger.addHandler(handler)
-app_logger.setLevel(logging.INFO)
-################################################################
+class Logger:
+    def __init__(self):
+        self.simple_formatter = logging.Formatter("[%(name)s] %(message)s")
+        self.complex_formatter = logging.Formatter(
+            "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] - %(message)s")
+        self.log_level = logging.INFO
+
+    def get_logger(self):
+        app_logger = getLogger()
+        handler = self.set_handler()
+        app_logger.addHandler(handler)
+        app_logger.setLevel(self.log_level)
+        return app_logger
+
+    def set_handler(self, ):
+        handler = logging.StreamHandler()
+        handler.setFormatter(self.complex_formatter)
+        return handler
+
+
+app_logger = Logger().get_logger()
+app_logger.propagate = False
