1d718a5a35fbda06edbe4ec5ec525460c568bbdd
arajo <i_tomorrow_i@hanmail.net>
2023-11-24 10:43:38 +0900

    Change log format

--- logger.py
+++ logger.py
@@ -3,7 +3,14 @@ from logging import getLogger
 
 ################################################################
 # Set logger
+simple_formatter = logging.Formatter("[%(name)s] %(message)s")
+complex_formatter = logging.Formatter(
+    "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] - %(message)s"
+)
+
 app_logger = getLogger()
-app_logger.addHandler(logging.StreamHandler())
+handler = logging.StreamHandler()
+handler.setFormatter(complex_formatter)
+app_logger.addHandler(handler)
 app_logger.setLevel(logging.INFO)
 ################################################################
